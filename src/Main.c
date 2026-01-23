de5a96c04105f5afe52e8339375e83b31fc4b11d
codeleaded <gnu.c.reaper@gmail.com>
2026-01-23 15:56:45 +0100

    init commit

---
+++ src/Main.c
@@ -0,0 +1,21 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+
+void Setup(AlxWindow* w){
+    
+}
+void Update(AlxWindow* w){
+    
+    Clear(BLACK);
+    
+    
+}
+void Delete(AlxWindow* w){
+
+}
+
+int main() {
+    if(Create("Graphics",1000,1000,1,1,Setup,Update,Delete)){
+        Start();
+    }
+    return 0;
+}
